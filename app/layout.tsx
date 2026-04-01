94526f929899353fb992f8d340166100e60f3371
Vannaroth Ngoc <vngoc@rgbarry.com>
2026-04-01 01:13:48 -0400

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import { EB_Garamond } from "next/font/google";
 import Header from "../ui/header";
@@ -39,6 +40,7 @@ export default function RootLayout({
         <Header />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
