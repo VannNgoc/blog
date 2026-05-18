178e6f16e1cc734898fc027f52027629aa35aa40
Vannaroth Ngoc <vannarothngoc@gmail.com>
2026-05-17 23:38:24 -0400

    Add basic Authentication

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,15 +1,35 @@
 import Link from "next/link";
+import { auth } from '@/lib/auth/server';
+import SignOutButton from './sign-out-button';
 
-export default function Header(){
-    return(
-        <div className="flex items-center justify-between p-4 bg-zinc-800 text-zinc-50">
-            <Link href="/" className="text-4xl font-medium tracking-wider text-zinc-50 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">
-                recollections
-            </Link>
-            <nav className="text-zinc-100">
-                <Link href="/posts" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Posts</Link>
-                <Link href="/login" className="underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Login</Link>
-            </nav>
-        </div>
-    )
+
+
+export default async function Header(){
+    const { data: session } = await auth.getSession();
+    if (session?.user) {
+        return (
+            <div className="flex items-center justify-between p-4 bg-zinc-800 text-zinc-50">
+                <Link href="/" className="text-2xl font-medium tracking-wider text-zinc-50 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">
+                    recollections <span className="text-sm text-zinc-400">of {session.user.name}</span>
+                </Link>
+                <nav>
+                    <Link href="/posts" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Posts</Link>
+                    <SignOutButton />
+                </nav>
+                
+            </div>
+        )
+    }else{
+        return(
+            <div className="flex items-center justify-between p-4 bg-zinc-800 text-zinc-50">
+                <Link href="/" className="text-2xl font-medium tracking-wider text-zinc-50 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">
+                    recollections
+                </Link>
+                <nav className="text-zinc-100">
+                    <Link href="/posts" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Posts</Link>
+                    <Link href="/auth/sign-in" className="underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Login</Link>
+                </nav>
+            </div>
+        )
+    }
 }
---
+++ ui/sign-out-button.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { authClient } from '@/lib/auth/client';
+
+export default function SignOutButton() {
+    return (
+        <button className="underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300" onClick={() => authClient.signOut()
+            .then(() => {
+                window.location.href = '/';
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+        }>Sign Out</button>
+    );
+}
