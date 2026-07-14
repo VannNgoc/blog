f8d674abcf79eeee71bafcaea5e65339bf7bc5e3
Vannaroth Ngoc <vannarothngoc@gmail.com>
2026-07-14 11:15:11 -0400

    hamburger menu for mobile

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { auth } from '@/lib/auth/server';
-import SignOutButton from './sign-out-button';
+import { NavigationMenu } from '@/ui/NavigationMenu';
 
 export const dynamic = 'force-dynamic';
 
@@ -12,12 +12,7 @@ export default async function Header(){
                 <Link href="/" className="text-2xl font-medium tracking-wider text-zinc-50 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">
                     recollections <span className="text-sm text-zinc-400">of {session.user.name}</span>
                 </Link>
-                <nav>
-                    <Link href="/dashboard" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Dashbaord</Link>
-                    <Link href="/posts" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Shared Posts</Link>
-                    <SignOutButton />
-                </nav>
-                
+                <NavigationMenu isSignedIn={true} />
             </div>
         )
     }else{
@@ -26,10 +21,7 @@ export default async function Header(){
                 <Link href="/" className="text-2xl font-medium tracking-wider text-zinc-50 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">
                     recollections
                 </Link>
-                <nav className="text-zinc-100">
-                    <Link href="/posts" className="mr-4 underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Shared Posts</Link>
-                    <Link href="/auth/sign-in" className="underline-offset-4 hover:underline hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-300">Login</Link>
-                </nav>
+                <NavigationMenu isSignedIn={session?.user ? true : false} />
             </div>
         )
     }
